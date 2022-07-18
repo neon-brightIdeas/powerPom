3748768b4cfb8bbeaf675e00633ff7a94bec2515
Neon Bright Ideas <108292195+neon-brightIdeas@users.noreply.github.com>
2022-07-18 12:47:08 -0600

    setup error
    added trusted pypi.org

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ from sys import platform
 # j_TODO : File path should be redone eventually
 if platform == "linux" or platform == "linux2":
     print('Installing Proper Modules for Linux')
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "./Tools/requirements_linux.txt"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "./Tools/requirements_linux.txt", "--trusted-host", "pypi.org"])
 elif platform == "darwin":
     # OS X
     print('This program is not currently compatible with Macs')
 elif platform == "win32":
     # Windows...
     print('Installing Proper Modules for Windows')
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "./Tools/requirements_windows.txt"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "./Tools/requirements_windows.txt", "--trusted-host", "pypi.org"])
